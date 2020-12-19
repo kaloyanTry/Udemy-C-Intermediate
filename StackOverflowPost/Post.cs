b1b302b766ef4ea4dac9c162ca658b97210bbbd5
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-19 10:53:43 +0200

    StackOverflow Post
    Console App with 3.cs files

---
+++ StackOverflowPost/Post.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackPost
+{
+    class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationTime { get; set; }
+        public int Vote { get; private set; }
+
+        public void UpVote()
+        {
+            Vote++;
+        }
+
+        public void DownVote()
+        {
+            Vote--;
+        }
+    }
+}
---
+++ StackOverflowPost/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace StackPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type yes and you can vote:");
+            string typeChoice = Console.ReadLine();
+            Console.Clear();
+
+            if (typeChoice == "yes")
+            {
+                StackPost();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private static void StackPost()
+        {
+            Post post = new Post();
+            post.Description = "This is a test post";
+            post.CreationTime = DateTime.Now;
+            post.Title = "Test";
+            Console.WriteLine("You can vote: \"UP\" or \"down\" and you can \"display\" the votes or \"exit\"");
+
+            while (true)
+            {
+                string vote = Console.ReadLine();
+
+                if (vote == "UP")
+                {
+                    post.UpVote();
+                    Console.WriteLine($"You've voted UP times");
+                    
+                }
+                else if (vote == "down")
+                {
+                    post.DownVote();
+                    Console.WriteLine($"You've voted down times");
+                }
+                else if (vote == "display")
+                {
+                    Console.WriteLine($"The title is: {post.Title}");
+                    Console.WriteLine($"The post is: {post.Description}");
+                    Console.WriteLine(post.CreationTime);
+                    Console.WriteLine($"The current vote is: {post.Vote}");
+                }
+                else if (vote == "exit")
+                {
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid type");
+                }
+            }
+        }
+    }
+}
