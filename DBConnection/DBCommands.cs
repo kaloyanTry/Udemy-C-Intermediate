bf3e54cabfbc1483ef52395e850d71ff997860c4
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-20 21:11:53 +0200

    Add files via upload

---
+++ DBConnection/DBCommands.cs
@@ -0,0 +1,43 @@
+﻿namespace DbConnection
+{
+    public class DbCommand : DbConnection
+    {
+        public DbCommand(string stringConnection, string command, string dbms)
+            : base(stringConnection, command)
+        {
+            if (_isValidConnection == true)
+            {
+                if (dbms == "sql")
+                {
+                    var sql = new SqlConnection(stringConnection);
+                    sql.Execute(command);
+                }
+                else if (dbms == "oracle")
+                {
+                    var oracle = new OracleConnection(stringConnection);
+                    oracle.Execute(command);
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("Please verify the parameters that you inserted. See the details that appear above. {0}", dbms);
+            }
+        }
+
+
+        public override void OpenConnection()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Execute(string command)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void CloseConnection()
+        {
+
+        }
+    }
+}
