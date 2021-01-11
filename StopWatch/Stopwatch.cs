f376f418d52924746ec4425332329d068ca9d3a6
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-11 21:02:02 +0200

    Update Stopwatch.cs

--- StopWatch/Stopwatch.cs
+++ StopWatch/Stopwatch.cs
@@ -13,22 +13,28 @@ namespace StopWatch
         public void Start()
         {
             _timeStart = DateTime.Now;
+            
             if (_running == true)
             {
                 throw new System.InvalidOperationException("The timer cannot be started while it is running");
             }
+            
             Console.WriteLine("The timer is running");
+            
             _running = true;
         }
 
         public void Stop()
         {
             _timeStart = DateTime.Now;
+            
             if (_running == false)
             {
                 throw new System.InvalidOperationException("The timer cannot be stopped while it is not running");
             }
+            
             _running = false;
+            
             Console.WriteLine("The timer has stopped at: " + (_timeStart - _timeStop));
         }
     }
